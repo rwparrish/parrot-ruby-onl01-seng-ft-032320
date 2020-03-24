19a5cc6588cd202a6e23703593a1f0d359f3036d
Ryan Parrish <rino3717@gmail.com>
2020-03-24 01:15:03 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot ("Pretty bird!", phrase ="Sqawk!")
+def parrot ("Pretty bird!", phrase ="Squawk!")
   puts 
   puts "#{phrase}"
 end
